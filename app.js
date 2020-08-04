78286899483b88ea7cdcfea911f0fef970056423
morikouki <akzouksia@icloud.com>
2020-08-04 11:28:53 +0900

    [Add]JavaScript-2

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var hello = 'Hello World';
-alert(hello);
+var orange = 100;
+var apple = 120;
+
+if (orange < apple) {
+	alert('みかんの値段がりんごより安い');
+}
+else if (orange == apple) {
+	alert('みかんがりんごが同じ値段');
+}
+else {
+	alert('みかんの値段がりんごより高い');
+}
